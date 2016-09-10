07374c0b16e1f94363978ef4dc4ec7145d84fafb
Federico Marcos <marcosfede@gmail.com>
2016-09-10 10:18:53 -0300

    almost done

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
 let vm = new Vue({
   el: '#app',
   data: {
-    greeting: 'Welcome to your Vue.js app!',
-    weather: ''
+    weather: '',
+    units: 'C'
+  },
+  created: function () {
+    this.getGeolocation()
+  },
+  watch: {
+    units: 'getGeolocation'
   },
   methods: {
-    getGeolocation: () => {
+    'getGeolocation': function () {
       if ('geolocation' in navigator) {
         navigator.geolocation.getCurrentPosition((position) => {
           let api = `http://api.openweathermap.org/data/2.5/weather?`
           let lat = position.coords.latitude.toString()
           let long = position.coords.longitude.toString()
           let latlong = `lat=${lat}&lon=${long}`
-          let units = '&units=metric'
+          let units = vm.units === 'C' ? '&units=metric' : '&units=imperial'
           let appid = '&APPID=eac1a9c63fa08c4a84e04c2bacb20b15'
           vm.$http.get(api + latlong + units + appid)
             .then((response) => {
-              console.log(response)
               let data = response.body
               vm.weather = {
                 city: data.name,
                 country: data.sys.country,
                 temp_avg: data.main.temp,
                 temp_min: data.main.temp_min,
-                temp_max: data.main.temp_max
+                temp_max: data.main.temp_max,
+                weather_main: data.weather[ 0 ].main,
+                weather_description: data.weather[ 0 ].description
               }
             }, (response) => {
               console.log('error', response)
@@ -32,6 +39,9 @@ let vm = new Vue({
       } else {
         window.alert('geolocation not found')
       }
+    },
+    'changeUnits': function () {
+      this.units = this.units == 'C' ? 'F' : 'C'
     }
   }
 })
